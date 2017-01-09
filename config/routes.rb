169626ef9419a361de79b6cb2315f1bf0289eeb6
Shinji Okamura <officeokamurakn.google.com>
2017-01-09 14:42:11 +0900

    Added single_product.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
           # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'potepan/index'
   get 'potepan/product_grid_left_sidebar'
+  get 'potepan/single_product'
 end
